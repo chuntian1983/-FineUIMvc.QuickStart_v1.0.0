b1933d52100b1626eb3dc9fc826ec8f80ab05b4e
chuntian1983 <wh25319755@qq.com>
2017-09-12 09:25:27 +0800

    2017-9-12

---
+++ FineUIMvc.QuickStart/Models/NewsType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FineUIMvc.QuickStart.Models
+{
+    public class NewsType
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "类别名称")]
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string lbname { get; set; }
+
+        [Display(Name = "排序号")]
+        [Required]
+        public int strord { get; set; }
+
+
+        [Display(Name = "父id")]
+        [Required]
+        [StringLength(200)]
+        public string parid { get; set; }
+
+        
+    }
+}
---
+++ FineUIMvc.QuickStart/Models/NewsTypeDbcontext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FineUIMvc.QuickStart.Models
+{
+    public class NewsTypeDbcontext: DbContext
+    {
+        public NewsTypeDbcontext(): base("DefaultConnection")
+        {
+        }
+
+        public DbSet<NewsType> NewsTypes { get; set; }
+    }
+}
